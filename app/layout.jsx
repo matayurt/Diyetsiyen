6333673f39ecd10e29a26aa921c97d071d3cbd1f
Mehmet <matayurt01@gmail.com>
2024-11-23 16:05:41 +0200

    VercelInsight

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Navbar from '@/components/Navbar'
 import { RecipeProvider } from '@/contexts/RecipeContext';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Modern Website',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
         <RecipeProvider>
         <Navbar />
           {children}
+          <SpeedInsights />
         </RecipeProvider>
       </body>
     </html>
